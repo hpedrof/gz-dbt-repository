a8b5fde97a0b4f2dba8427c7bfd0fa2da489b03a
Pedro Rocha <hfrocha.pedro@gmail.com>
2023-11-01 19:27:48 +0000

    adwwords

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -12,8 +12,8 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name,
-        ads_cost,
+        campgn_name as campaign_name,
+        ads_cost, cast(ads_cost as float64)
         impression,
         click
 
